594a148b03052d7e88591a1ec5361d0b2c8d62c6
mdmunir <misbahuldmunir@gmail.com>
2025-10-18 10:24:12 +0700

    Add column filter to component GridView

--- resources/client/app.js
+++ resources/client/app.js
@@ -1,5 +1,5 @@
 import vuetify from './plugins/vuetify';
-import mainPlugin from './plugins/main';
+import main from './plugins/main';
 import InitApp from 'virtual:yii2-inertia';
 
 import 'vuetify/lib/styles/main.sass';
@@ -8,7 +8,7 @@ import './assets/css/app.css';
 InitApp({
     id: 'app',
     setup(app) {
-        app.use(mainPlugin)
+        app.use(main)
             .use(vuetify);
     }
 });
